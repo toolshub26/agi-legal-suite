1d9583836e0a16b99a2952291145b11f04513629
toolshub26 <tools.hub26@gmail.com>
2026-04-21 18:57:25 +0530

    Create sw.js
    Added full PWA support to the project.
    
    - Created manifest.json for installable app
    - Added service worker (sw.js) for offline caching
    - Enabled standalone mobile app experience
    - Improved performance and loading speed
    - Prepared app for future enhancements
    
    This update allows users to install the app on mobile and use it like a native application with offline support.

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "app-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache=>{
+      return cache.addAll([
+        "/",
+        "index.html",
+        "dashboard.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
+});
