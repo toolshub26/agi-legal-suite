ac9e36f54e928f434e372019e8841e54a273ba00
toolshub26 <tools.hub26@gmail.com>
2026-06-01 08:03:47 +0530

    feat: connect affidavit templates database

--- js/templates.js
+++ js/templates.js
@@ -1,1 +1,11 @@
+async function loadTemplates() {
+  try {
+    const response = await fetch("data/affidavit-types.json");
+    const templates = await response.json();
 
+    console.log("Templates Loaded:", templates.length);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
